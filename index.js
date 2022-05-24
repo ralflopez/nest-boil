1034837dd07102e9a7063244d2a9f8e503f6a808
Ralf <ralflopez3@gmail.com>
2022-05-24 09:22:34 +0800

    :necktie: read command line args

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
+import fs from "fs"
 
-async function main() {}
+async function main() {
+  const args = process.argv
+  if (args.length < 3) {
+    console.log(`Usage: npx nest-boil <FILE_PATH>`)
+    process.exit(1)
+  }
+
+  const filePath = process.argv[2]
+
+  try {
+    const schema = fs.readFileSync(filePath, "utf-8")
+    console.log(schema)
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 main()
